e9da89c3275eb83e8d669ebc1a27d6fd5b0b2e9d
frances conley wood <f.conleywood@gmail.com>
2016-01-14 09:51:42 -0500

    Exercise 1 Add Methods

---
+++ Class Time.rb
@@ -0,0 +1,15 @@
+# Let's start by creating two classes: one called Student and another called
+# Instructor.
+class Student
+  def self.learn
+    puts "I get it!"
+end
+
+class Instructor
+  def self.teach
+    puts "Everything in Ruby is an Object".
+end
+
+# The student class has a method called learn that outputs "I get it!".
+# The instructor class has a method called teach that outputs 
+# "Everything in Ruby is an Object".

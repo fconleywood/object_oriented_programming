f11db3db78a612bafe15a1a661598f1ec9c191fb
frances conley wood <f.conleywood@gmail.com>
2016-01-14 10:10:43 -0500

    Exercise 1 Create Parent Class

--- Class Time.rb
+++ Class Time.rb
@@ -1,15 +1,22 @@
 # Let's start by creating two classes: one called Student and another called
 # Instructor.
-class Student
+class Person
+   attr_accessor :name
+ end
+
+class Student < Person
   def self.learn
     puts "I get it!"
 end
 
-class Instructor
+class Instructor < Person
   def self.teach
     puts "Everything in Ruby is an Object".
 end
 
 # The student class has a method called learn that outputs "I get it!".
-# The instructor class has a method called teach that outputs 
+# The instructor class has a method called teach that outputs
 # "Everything in Ruby is an Object".
+
+# Create a parent Person class that contains the attribute name and an
+# initializer to set the name.

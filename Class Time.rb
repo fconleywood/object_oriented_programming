3cdd0347df2328b2f1c2251956dca22d3580df76
frances conley wood <f.conleywood@gmail.com>
2016-01-14 13:08:14 -0500

    Exercise 1 Call Methods

--- Class Time.rb
+++ Class Time.rb
@@ -2,17 +2,36 @@
 # Instructor.
 class Person
    attr_accessor :name
- end
+
+   def greeting
+      "Hi, my name is #{name}"
+    end
+  end
+
 
 class Student < Person
-  def self.learn
+  def learn
     puts "I get it!"
+  end
 end
 
+  cristina = Student.new
+  cristina.name = "Cristina"
+
+
 class Instructor < Person
-  def self.teach
-    puts "Everything in Ruby is an Object".
+  def teach
+    puts "Everything in Ruby is an Object"
+  end
 end
+
+  chris = Instructor.new
+  chris.name = "Chris"
+
+chris.teach
+
+cristina.teach
+
 
 # The student class has a method called learn that outputs "I get it!".
 # The instructor class has a method called teach that outputs
@@ -20,3 +39,9 @@ end
 
 # Create a parent Person class that contains the attribute name and an
 # initializer to set the name.
+
+# Both the instructor and the student should also be able to do a greeting,
+# like "Hi, my name is #{name}".
+
+#Create an instance of Instructor whose name is "Chris" and call his greeting.
+#Create an instance of Student whose name is "Cristina" and call her greeting.

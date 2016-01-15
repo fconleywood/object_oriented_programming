448776ef163c2ee00d728b6fbb074610e0ffb10b
frances conley wood <f.conleywood@gmail.com>
2016-01-15 16:58:15 -0500

    layout update

--- rover.rb
+++ rover.rb
@@ -1,21 +1,21 @@
 
 class Rover
+  attr_accessor :x, :y, :dir
 
-attr_accessor :x :y :dir
-
-def initialize=(x, y, direction)
-  @x = x
-  @y = y
-  @direction = direction
-end
+  def initialize(x, y, direction)
+    @x = x
+    @y = y
+    @direction = direction
+  end
 
 def read_instruction(instructions)
   instructions_list = instructions.split("")
+  # puts "#{@x}, #{@y}, #{@heading}"
   instructions_list.each do |action|
     if action == "M"
-      move
-    elsif
-      turn
+      move(action)
+    elsif action == "L" || "R"
+      turn(action)
     else
       puts "Try again!"
     end
@@ -53,43 +53,53 @@ end
 
 def move(direction)
   case direction
-  when @direction == "N" { @x += 1 } #if answer is beigger > then b plateau == 0
-  when @direction == "E" { @y += 1 }
-  when @direction == "S" { @x -= 1 }
-  when @direction == "W" { @y -= 1 }
+  when @direction == "N" { @y += 1 } #if answer is beigger > then b plateau == 0
+  when @direction == "E" { @x += 1 }
+  when @direction == "S" { @y -= 1 }
+  when @direction == "W" { @x -= 1 }
 end
+end
+# def test
+#   testrover = Rover.new(1,2,"N")
+#   testrover.read_instruction("LMLMLMLMM")
+# end
 
 #I need to output the information of new coordinates
 
-end
+# rover = Rover.new
+# rover = "Rover One"
 
-rover = Rover.new
-rover = "Rover One"
+    puts "Enter plateau size: "
+    size = gets.chomp
+    size = size.split("").to_i
 
-puts "Enter plateau size: "
-size = gets.chomp
-size = size.split(a,b)
+    puts "Rover One: Enter my x,y coordinates and compass orientation using N,W,E or S: "
+    location_one = gets.chomp
+    location_one = location_one.upcase
+    read_instruction(location_one)
+
+    puts "Enter your Rover One's action using L, M, R: "
+    rover1_move = gets.chomp
+    rover1_move = action_one.upcase
+    read_instruction(rover1_move)
+
+    puts "Rover Two: Enter my x,y coordinates and compass orientation using N,W,E or S: "
+    location_two = gets.chomp
+    location_two = location_two.upcase
+    read_instruction(location_two)
+
+    puts "Enter your Rover Two's action using L, M, R: "
+    rover2_move = gets.chomp
+    rover2_move = rover2_move.upcase
+    read_instruction(rover2_move)
 
 
+    rover1 = Rover.new(rover1_move[0].to_i,rover1_move[1].to_i,rover1_move[2])
+    rover2 = Rover.new(rover2_move[0].to_i,rover2_move[1].to_i,rover2_move[2])
 
-puts "Enter your Rover One's x,y location and which direction it is facing: "
-location_one = gets.chomp
-location_one = location_one.upcase
-read_instruction(location_one)
+    rover1.read_instruction(rover1_instructions)
+    rover2.read_instruction(rover2_instructions)
 
-puts "Enter your Rover One's  action using L, M, R: "
-action_one = gets.chomp
-action_one = action_one.upcase
-read_instruction(action_one)
-
-
-puts "Enter your Rover Two's x,y location and which direction it is facing: "
-location_two = gets.chomp
-location_two = location_two.upcase
-read_instruction(location_two)
-#Rover.new
-
-puts "Enter your Rover Two's action using L, M, R: "
-action_two = gets.chomp
-action_two = action_two.upcase
-read_instruction(action_two)
+    puts "Rover 1 is located: #{rover1.x} #{rover1.y} #{rover1.heading}"
+    puts "Rover 2 is located: #{rover2.x} #{rover2.y} #{rover2.heading}"
+  end
